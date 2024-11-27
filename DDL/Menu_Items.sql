faed5dab85b7ad5ef904eb7d6dbf5df21d5c1f0e
Indrani Borra <indraniborra@IndraniMac.local>
2024-11-27 12:07:52 -0600

    Modified Menu_Items and Restaurant_Owner

--- DDL/Menu_Items.sql
+++ DDL/Menu_Items.sql
@@ -7,3 +7,6 @@ CREATE TABLE Menu_Items (
   CONSTRAINT Menu_Items_PK PRIMARY KEY (Item_Id),
   CONSTRAINT Menu_Items_Menu_Item_Category_FK FOREIGN KEY (Item_Category) REFERENCES Menu_Item_Category (Item_Category)
 );
+-- add column  Item_Created_By_User_Id to the table with datatype INT and and forign key from Restaurant_Owner table
+ALTER TABLE Menu_Items ADD Item_Created_By_User_Id INT;
+ALTER TABLE Menu_Items ADD CONSTRAINT Menu_Items_Restaurant_Owner_FK FOREIGN KEY (Item_Created_By_User_Id) REFERENCES Restaurant_Owner (User_Id);
--- DDL/Restaurant_Owner.sql
+++ DDL/Restaurant_Owner.sql
@@ -1,4 +1,11 @@
 CREATE TABLE Restaurant_Owner (
   User_Id varchar(100) DEFAULT NULL 
   CONSTRAINT Restaurant_Owner_Users_FK FOREIGN KEY (User_Id) REFERENCES Users (User_Id)
-) 
+);
+-- chnage the User_Id to INT
+ALTER TABLE Restaurant_Owner MODIFY User_Id INT;
+-- User_Id is the primary key
+ALTER TABLE Restaurant_Owner ADD CONSTRAINT Restaurant_Owner_PK PRIMARY KEY (User_Id);
+-- Add column Restaurant_Id to the table and forign key to Restaurants table
+ALTER TABLE Restaurant_Owner ADD Restaurant_Id INT;
+ALTER TABLE Restaurant_Owner ADD CONSTRAINT Restaurant_Owner_Restaurant_FK FOREIGN KEY (Restaurant_Id) REFERENCES Restaurants (Restaurant_Id);
