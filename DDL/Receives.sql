b71fa6ad1c5068beaa9eca7f2116a12e7a32b693
Indrani Borra <indraniborra@IndraniMac.local>
2024-11-27 12:46:37 -0600

    Modified some DDL files

--- DDL/Receives.sql
+++ DDL/Receives.sql
@@ -5,3 +5,5 @@ CREATE TABLE Receives (
   CONSTRAINT Receives_Restaurants_FK FOREIGN KEY (Restaurant_Id) REFERENCES Restaurants (Restaurant_Id),
   CONSTRAINT Receives_Orders_FK FOREIGN KEY (Order_Id) REFERENCES Orders (Order_Id)
 );
+-- change table name to Restaurant_Orders
+RENAME TABLE Receives TO Restaurant_Orders;
